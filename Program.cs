feedca5b96d919275e1f3f9a277815955b114873
RealPhonki <phonkibusiness@gmail.com>
2026-03-27 21:34:17 -0400

    Added env files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Biasfish.Core;
+using System;
+using System.Reflection.Metadata;
+
+namespace Biasfish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board testBoard = new Board();
+            Console.WriteLine(testBoard);
+        }
+    }
+}
--- core/Board.cs
+++ core/Board.cs
@@ -23,5 +23,40 @@ namespace Biasfish.Core
         public int castlingRights;
         public int halfMoveClock;
         public ulong key;
+
+        public ulong GetBitboard(int pieceType)
+        {
+            switch (pieceType)
+            {
+                case Piece.WhitePawns:   return whitePawns;
+                case Piece.WhiteKnights: return whiteKnights;
+                case Piece.WhiteBishops: return whiteBishops;
+                case Piece.WhiteRooks:   return whiteRooks;
+                case Piece.WhiteQueens:  return whiteQueens;
+                case Piece.BlackPawns:   return blackPawns;
+                case Piece.BlackKnights: return blackKnights;
+                case Piece.BlackRooks:   return blackRooks;
+                case Piece.BlackQueens:  return blackQueens;
+                case Piece.BlackKings:   return blackKings;
+                default: return 0;
+            }
+        }
+
+        public void SetBitboard(int pieceType, ulong value)
+        {
+            switch (pieceType)
+            {
+                case Piece.WhitePawns:   whitePawns   = value; break;
+                case Piece.WhiteKnights: whiteKnights = value; break;
+                case Piece.WhiteBishops: whiteBishops = value; break;
+                case Piece.WhiteRooks:   whiteRooks   = value; break;
+                case Piece.WhiteQueens:  whiteQueens  = value; break;
+                case Piece.BlackPawns:   blackPawns   = value; break;
+                case Piece.BlackKnights: blackKnights = value; break;
+                case Piece.BlackRooks:   blackRooks   = value; break;
+                case Piece.BlackQueens:  blackQueens  = value; break;
+                case Piece.BlackKings:   blackKings   = value; break;
+            }
+        }
     }
 }
